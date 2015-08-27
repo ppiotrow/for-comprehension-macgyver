f20809f8b43f4cfa59ef0b04e1bcb5c41fb73220
p.piotrowski <p.piotrowski@adform.com>
2015-08-27 16:08:40 +0200

    Merge two files into one

--- src/main/scala/pl/codepot/common/Tiramisu.scala
+++ src/main/scala/pl/codepot/common/Tiramisu.scala
@@ -1,7 +1,27 @@
 package pl.codepot.common
 
+case class Water(temperature: Int)
+
+case class Beans(kind: String)
+
+case class Espresso(isSweet: Boolean = false) {
+  def sweeten = copy(isSweet = true)
+}
 case class Cookies(number: Int)
 case class Tiramisu()
+
+object CoffeeMachine {
+  def espresso(water: Water, beans: Beans): Either[String, Espresso] = {
+    if (water.temperature > 100)
+      Left("Water too hot")
+    else if (water.temperature < 93)
+      Left("Water too cold")
+    else if (!beans.kind.equals("Robusta"))
+      Left("Wrong type of beans")
+    else
+      Right(Espresso())
+  }
+}
 
 object TiramisuFactory {
   def create(cookies: Cookies, espresso: Espresso): Either[String, Tiramisu] = {
@@ -13,3 +33,4 @@ object TiramisuFactory {
       Right(Tiramisu())
   }
 }
+
